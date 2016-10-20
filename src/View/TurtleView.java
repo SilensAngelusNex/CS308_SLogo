261f35b7f6f8fdc5b9f879ab9d08c082dd676d21
Weston Carvalho <wac12@duke.edu>
2016-10-20 15:35:40 -0400

    Start preliminary work on Model.

---
+++ src/View/TurtleView.java
@@ -0,0 +1,16 @@
+package View;
+
+import java.awt.Point;
+
+public interface TurtleView {
+
+	abstract public Point getCurrentLocation();
+	
+	abstract public double getCurrentHeading();
+	
+	abstract public Point getPreviousLocation();
+	
+	abstract public double getPreviousHeading();
+	
+	abstract public String getImagePath();
+}
