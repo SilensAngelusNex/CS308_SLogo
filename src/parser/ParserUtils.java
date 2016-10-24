fc4c2c2e7a3878d3b13c438e57728047a9d7f4cb
Daniel Chai <dhc10@duke.edu>
2016-10-23 21:47:50 -0400

    Extend parser to handle more cases

--- src/parser/ParserUtils.java
+++ src/parser/ParserUtils.java
@@ -11,6 +11,8 @@ public class ParserUtils {
 	
 	public static final String SYNTAX_FILE_PATH = "resources/languages/Syntax";
 	
+	public static final String NUM_PARAMS_PATH = "resources/CommandNumParams";
+	
 	public static final String ERROR_CODE = "NO MATCH";
 	public static final String UNKNOWN_COMMAND_CODE = "UNKNOWN COMMAND";
 }
