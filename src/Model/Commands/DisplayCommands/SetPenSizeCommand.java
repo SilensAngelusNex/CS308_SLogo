368afc35540461afcedbeac7f1460f11d1efe94f
Daniel Chai <dhc10@duke.edu>
2016-10-30 20:07:42 -0400

    Add DislpayCommand classes

---
+++ src/Model/Commands/DisplayCommands/SetPenSizeCommand.java
@@ -0,0 +1,33 @@
+package Model.Commands.DisplayCommands;
+
+import java.util.ResourceBundle;
+
+import Model.CommandableModel;
+import Model.Commands.AbstractCommand;
+import parser.InvalidCommandException;
+
+public class SetPenSizeCommand extends AbstractCommand {
+
+	public SetPenSizeCommand(CommandableModel model, ResourceBundle language) {
+		super(model, language);
+	}
+
+	@Override
+	public int maxArgs() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public String getName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected double execCommand() throws InvalidCommandException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
