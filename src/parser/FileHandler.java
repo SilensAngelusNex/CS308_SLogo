e8c83455d5dfcf18132b972a41f63419b11991df
Daniel Chai <dhc10@duke.edu>
2016-10-30 21:50:32 -0400

    Extract file handling to a FileHandler clasS

---
+++ src/parser/FileHandler.java
@@ -0,0 +1,38 @@
+package parser;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+import Model.Commands.Command;
+
+public class FileHandler {
+	private static final String END_OF_FILE = "\\z";
+	
+	private MainParser myParser;
+	
+	public FileHandler(MainParser myParser) {
+		this.myParser = myParser;
+	}
+	
+	/**
+     * Returns the ExpressionTree for the contents in a file
+     * @throws FileNotFoundException 
+     */
+    public Command getExpressionTreeFromFile(String filePath) throws FileNotFoundException {
+    	String fileInput = readFileToString(filePath);
+    	return myParser.getExpressionTreeFromCommand(fileInput);
+    }
+    
+    /**
+	 * Reads given file and returns its entire contents as a single string
+	 */
+    private String readFileToString(String filename) throws FileNotFoundException {
+        Scanner input = new Scanner(new File(filename));
+        input.useDelimiter(END_OF_FILE);
+        String result = input.next();
+        input.close();
+        
+        return result;
+    }
+}
--- src/parser/MainParser.java
+++ src/parser/MainParser.java
@@ -1,8 +1,5 @@
 package parser;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.Scanner;
 import java.util.Stack;
 
 import Model.CommandableModel;
@@ -14,12 +11,10 @@ import Model.Commands.*;
  * 
  * @author Daniel Chai
  * @author Weston
- *
  */
 public class MainParser {
 	private static final String NEWLINE = "\\s*\\r?\\n\\s*";
 	private static final String WHITESPACE = "\\p{Space}";
-	private static final String END_OF_FILE = "\\z";
 	
 	private static final String COMMENT_CODE = "Comment";
 	private static final String COMMAND_CODE = "Command";
@@ -41,27 +36,6 @@ public class MainParser {
      */
     public Command getExpressionTreeFromCommand(String command) {
         return createExpressionTree(myParser, command.split(NEWLINE));
-    }
-    
-    /**
-     * Returns the ExpressionTree for the contents in a file
-     * @throws FileNotFoundException 
-     */
-    public Command getExpressionTreeFromFile(String filePath) throws FileNotFoundException {
-    	String fileInput = readFileToString(filePath);
-    	return getExpressionTreeFromCommand(fileInput);
-    }
-    
-    /**
-	 * Reads given file and returns its entire contents as a single string
-	 */
-    private String readFileToString(String filename) throws FileNotFoundException {
-        Scanner input = new Scanner(new File(filename));
-        input.useDelimiter(END_OF_FILE);
-        String result = input.next();
-        input.close();
-        
-        return result;
     }
     
     /**
