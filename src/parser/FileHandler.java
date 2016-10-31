c618f870e9153b461780556105d67920202f030f
Daniel Chai <dhc10@duke.edu>
2016-10-30 22:57:09 -0400

    Fix formatting

--- src/parser/FileHandler.java
+++ src/parser/FileHandler.java
@@ -7,37 +7,39 @@ import java.util.Scanner;
 import Model.Commands.Command;
 
 /**
- * This class handles reading commands from a file and saving commands to a file.
+ * This class handles reading commands from a file and saving commands to a
+ * file.
  * 
  * @author Daniel Chai
  */
 public class FileHandler {
 	private static final String END_OF_FILE = "\\z";
-	
+
 	private MainParser myParser;
-	
+
 	public FileHandler(MainParser myParser) {
 		this.myParser = myParser;
 	}
-	
+
 	/**
-     * Returns the ExpressionTree for the contents in a file
-     * @throws FileNotFoundException 
-     */
-    public Command getExpressionTreeFromFile(String filePath) throws FileNotFoundException {
-    	String fileInput = readFileToString(filePath);
-    	return myParser.getExpressionTreeFromCommand(fileInput);
-    }
-    
-    /**
+	 * Returns the ExpressionTree for the contents in a file
+	 * 
+	 * @throws FileNotFoundException
+	 */
+	public Command getExpressionTreeFromFile(String filePath) throws FileNotFoundException {
+		String fileInput = readFileToString(filePath);
+		return myParser.getExpressionTreeFromCommand(fileInput);
+	}
+
+	/**
 	 * Reads given file and returns its entire contents as a single string
 	 */
-    private String readFileToString(String filename) throws FileNotFoundException {
-        Scanner input = new Scanner(new File(filename));
-        input.useDelimiter(END_OF_FILE);
-        String result = input.next();
-        input.close();
-        
-        return result;
-    }
+	private String readFileToString(String filename) throws FileNotFoundException {
+		Scanner input = new Scanner(new File(filename));
+		input.useDelimiter(END_OF_FILE);
+		String result = input.next();
+		input.close();
+
+		return result;
+	}
 }
