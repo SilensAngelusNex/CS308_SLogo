ec18d3fb562220aeadaec2c8def9033b7bdd4787
Blake Becerra <bab72@duke.edu>
2016-11-07 10:55:50 -0500

    Continue documenting code

--- src/View/UIFactory.java
+++ src/View/UIFactory.java
@@ -18,11 +18,11 @@ import javafx.scene.control.Alert.AlertType;
 
 public class UIFactory {
 	private ResourceBundle myUILabel;
-	
+
 	protected UIFactory(ResourceBundle uilabel){
 		myUILabel = uilabel;
 	}
-	
+
 	protected Button makeButton (String property, EventHandler<ActionEvent> handler) {
 		Button result = new Button();
 		String label = myUILabel.getString(property);
@@ -30,13 +30,13 @@ public class UIFactory {
 		result.setOnAction(handler);
 		return result;
 	}
-	
+
 	protected ChoiceBox<String> makeChoiceBox(ObservableList<String> choices, String Type){
 		ChoiceBox<String> retBox = new ChoiceBox<String>(choices);
 		if(Type.equals("Language")){
 			retBox.getSelectionModel().selectFirst();
 			retBox.setTooltip(new Tooltip("Select the language"));
-			
+
 		}
 		else if(Type.equals("Color")){
 			retBox.getSelectionModel().selectFirst();
@@ -48,11 +48,11 @@ public class UIFactory {
 		}
 		return retBox;
 	}
-	
+
 	protected Label makeLabel(String labeltext){
 		return new Label();
 	}
-	
+
 	protected void promptAlert(String s, Exception e){
 		Alert alert = new Alert(AlertType.ERROR);
 		alert.setTitle(s);
